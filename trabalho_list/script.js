ffdc433e2162142bfdb0172eda2e82a8cbe146fc
Alison lemes <alison.pereira@eaportal.org>
2024-08-20 21:16:45 +0000

    teste1

--- trabalho_list/script.js
+++ trabalho_list/script.js
@@ -1,44 +1,60 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const addButton = document.querySelector('.Adicionar');
-    const inputField = document.querySelector('input[type="text"]');
-    const todoList = document.querySelector('.todo-list');
 
-    // Adicionar uma nova tarefa
-    addButton.addEventListener('click', function() {
-        const taskText = inputField.value.trim();
-        if (taskText !== '') {
-            const newTask = document.createElement('li');
-            newTask.innerHTML = `
-                <input type="checkbox">
-                <label>${taskText}</label>
-                <button class="edit">
-                    <img src="./img/cached_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.png" alt="Editar" title="Editar">
-                </button>
-                <button class="delete">
-                    <img src="./img/delete_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.png" alt="Apagar" title="Apagar">
-                </button>
-            `;
-            todoList.appendChild(newTask);
-            inputField.value = ''; // Limpa o campo de texto
-        }
-    });
+let respostaAPI = [
+    {"id": 1, "descrição": "varrer a sala "},
+    {"id": 2, "descrição": "revisar a aula de BD"},
+    {"id": 3, "descrição": "lavar a louça"},
+    {"id": 4, "descrição": "lavar a roupa"},
+    {"id": 5, "descrição": "Arrumar a cama"}   //json
+];
+//array matriz
+const taskList = document.getElementById("task-list");
 
-    // Editar ou deletar uma tarefa existente
-    todoList.addEventListener('click', function(e) {
-        const taskItem = e.target.closest('li');
-        
-        // Deletar tarefa
-        if (e.target.closest('.delete')) {
-            taskItem.remove();
-        }
-        
-        // Editar tarefa
-        if (e.target.closest('.edit')) {
-            const label = taskItem.querySelector('label');
-            const newTaskText = prompt("Edite a tarefa:", label.textContent);
-            if (newTaskText !== null && newTaskText.trim() !== '') {
-                label.textContent = newTaskText;
-            }
-        }
-    });
-});
+function init(){
+    carregarLista()
+}
+
+function carregarLista(){
+    let idx = -1;
+    const listContainer = document.getElementById("task-list");
+    listContainer.innerHTML= '';
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function salvarTarefa(){
+//     const Adicionar = document.getElementById("adicionar")
+//     repostaAPI.push({"id: getProximoId(), "descricao": Adicionar.value});
+//     Adicionar.value = ''
+//     carregarLista();
+// }
+
+
+// function getProximoId(){
+//     return 1;
+// }
+
+// Function editarTarefa()
